7cc5c366e7c13fa5941c98bce0d68a15e31aaf86
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2023-06-14 11:19:39 +0900

    Create 383. Ransom Note.cpp

---
+++ LeetCode/383. Ransom Note.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+     int r_alpha[26] ={0}; int m_alpha[26] =  {0};
+     for(int i=0;i<ransomNote.size();i++)
+        r_alpha[ransomNote[i] - 'a']++;
+
+     for(int i=0;i<magazine.size();i++)
+        m_alpha[magazine[i] - 'a']++;
+       
+     for(int i=0;i <sizeof(r_alpha)/sizeof(int);i++){
+         if(r_alpha[i] > m_alpha[i])
+            return false;
+     }
+     return true;
+    }
+};
+
+/* 
+c++ 배열에는 size()함수 없가 없다. sizeof(배열이름)/sizeof(배열형)을 사용하자
+내가 작성한 위 코드에서 메모리를 줄일 수 있을 것 같다. 다른 방법을 생각해보자
+*/

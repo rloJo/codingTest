5ba9cd12fc6978f1386c172940da2999e2fb91e4
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2023-06-22 12:13:01 +0900

    Create 거꾸로 출력해 보아요.cpp

---
+++ SWEA/D1/거꾸로 출력해 보아요.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int N =0;
+  cin >> N;
+  for(int i=N; i>=0;i--){
+     cout << i << " ";   
+  }  
+  return 0;
+}

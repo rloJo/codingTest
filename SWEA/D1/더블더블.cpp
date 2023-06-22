d2ae57bad2a17203f7f95f7ccc6c5207342a72b2
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2023-06-22 12:10:39 +0900

    Create 더블더블.cpp

---
+++ SWEA/D1/더블더블.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int T =0;
+    cin >> T;
+    for(int i=0; i<=T;i++){
+      cout << (1<<i) << " ";   
+    }
+	  return 0;
+}

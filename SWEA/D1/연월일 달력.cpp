8dc89dc5312971207169555e86fba60843df648e
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2023-06-24 16:49:58 +0900

    Update 연월일 달력.cpp

--- SWEA/D1/연월일 달력.cpp
+++ SWEA/D1/연월일 달력.cpp
@@ -7,30 +7,30 @@ int main(int argc, char** argv)
 {
     bool flag = true ;
     int last_day[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	int test_case;
-	int T;
-  	int date;
+    int test_case;
+    int T;
+    int date;
     int Y,M,D;
-	cin>>T;
-	for(test_case = 1; test_case <= T; ++test_case)
-	{
-        flag = true;
-		cin >> date;
-        D = date%100;
-        date/=100;
-        M = date%100;
-        date /= 100;
-        Y = date;
+    cin>>T;
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+    	flag = true;
+   	cin >> date;
+    	D = date%100;  
+    	date/=100;
+   	M = date%100;
+    	date /= 100;
+    	Y = date;
 
-		if(M<1 || M >12)
-            flag = false;
-        else if(last_day[M-1] < D || D < 1)
-            flag = false;
-        cout << "#" << test_case << " " ;
-        if(flag)
+    	if(M<1 || M >12)
+        	flag = false;
+    	else if(last_day[M-1] < D || D < 1)
+        	flag = false;
+    	cout << "#" << test_case << " " ;
+    	if(flag)
         	cout << setw(4) << setfill('0') <<Y << "/" << setw(2) << setfill('0') <<M << "/" << setw(2) << setfill('0') << D << "\n";
-        else 
-            cout << "-1" << "\n";
-	}
-	return 0;
+    	else 
+        	cout << "-1" << "\n";
+    }
+    return 0;
 }

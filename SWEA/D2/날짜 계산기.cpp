7b08fc6c8116fbc447a46eaf9c5bf8293a778169
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2023-06-24 16:01:02 +0900

    Update 날짜 계산기.cpp

--- SWEA/D2/날짜 계산기.cpp
+++ SWEA/D2/날짜 계산기.cpp
@@ -5,25 +5,25 @@ using namespace std;
 int main(int argc, char** argv)
 {
   int last_day[12] = { 31, 28, 31, 30 ,31, 30, 31, 31, 30, 31 , 30 ,31};
-	int test_case;
-	int T;
+  int test_case;
+  int T;
   int M1, D1, M2, D2;
   int dday;
-	cin>>T;
+  cin>>T;
 	
-	for(test_case = 1; test_case <= T; ++test_case)
-	{
-        dday = 0;
-		cin >> M1 >> D1 >> M2 >> D2; 
-        if(M1 == M2)
-            dday = D2 - D1 +1;
-        else {
-            dday += last_day[M1-1] - D1 + 1;
-            for(int i=M1; i<M2-1;i++)
-                dday+= last_day[i];
-            dday += D2;
-        }
-        cout << "#" << test_case << " " << dday << "\n";
-	}
-	return 0;
+  for(test_case = 1; test_case <= T; ++test_case)
+  {
+  	dday = 0;
+  	cin >> M1 >> D1 >> M2 >> D2; 
+  	if(M1 == M2)
+  		dday = D2 - D1 +1;
+ 	 else {
+ 		dday += last_day[M1-1] - D1 + 1;
+  		for(int i=M1; i<M2-1;i++)
+  			dday+= last_day[i];
+  		dday += D2;
+  	}
+  	cout << "#" << test_case << " " << dday << "\n";
+  }
+  return 0;
 }

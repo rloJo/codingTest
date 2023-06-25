00fda5e3e73c0814f4b587d3748757b2775fee6e
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2023-06-25 17:26:06 +0900

    Update and rename 쉬운 거스름돈,cpp to 쉬운 거스름돈.cpp

--- SWEA/D2/쉬운 거스름돈.cpp
+++ SWEA/D2/쉬운 거스름돈.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main(int argc, char** argv)
 {
   int money[8] = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
-	int test_case;
-	int T, N;
-	cin>>T;
-	for(test_case = 1; test_case <= T; ++test_case)
-	{
-		cin >> N;
+  int test_case;
+  int T, N;
+  cin>>T;
+  for(test_case = 1; test_case <= T; ++test_case)
+  {
+    cin >> N;
     cout<< "#" << test_case << "\n";
     for(int i=0; i<8;i++)
     {
@@ -18,6 +18,6 @@ int main(int argc, char** argv)
         N %= money[i];
     }
     cout <<"\n";
-	}
-	return 0;
+   } 
+   return 0;
 }

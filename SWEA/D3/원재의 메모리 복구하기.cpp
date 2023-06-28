66d8612170719c2051097bba8bc7745d9c031ee7
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2023-06-28 11:52:38 +0900

    Update 원재의 메모리 복구하기.cpp

--- SWEA/D3/원재의 메모리 복구하기.cpp
+++ SWEA/D3/원재의 메모리 복구하기.cpp
@@ -35,3 +35,39 @@ int main(int argc, char** argv)
 	}
 	return 0;
 }
+
+// 위 방법은 생각나는대로 푼 방법
+// 2중 for문 및 불필요한 str 변수를 사용한다
+// 위 문제를 해결해보자
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int check_bit(string bit) { //문제의 조건대로 비트를 체크 하는 함수 정의
+	int ans = 0; //정답 변수
+	bool flag = false;
+	for (int i = 0; i < bit.size(); i++)
+	{
+		if (flag != bit[i] - '0') {
+			ans++;
+			flag = !flag;
+		}
+	}
+	return ans;
+}
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	string bit;
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		cin >> bit;
+		cout << "#" << test_case << " " << check_bit(bit) << "\n";
+	}
+	return 0;
+}

04575aefbbfa9c8c493daba96dff326cc62e9f78
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2023-07-03 11:53:04 +0900

    Update [SW 문제해결 기본] 1일차 - View.cpp

--- SWEA/D3/[SW 문제해결 기본] 1일차 - View.cpp
+++ SWEA/D3/[SW 문제해결 기본] 1일차 - View.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	int test_case;
-	int T,N;
+	int T, N;
 	int max;
 	int count;
 	T = 10;
-	
+
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
 		count = 0;
@@ -18,21 +18,20 @@ int main(int argc, char** argv)
 		vector<int> building(N);
 		for (int i = 0; i < N; i++)
 			cin >> building[i];
-		for (int i = 2; i < N-2; i++)
+		for (int i = 2; i < N - 2; i++)
 		{
 			max = 0;
+			if (building[i - 2] < building[i] && building[i + 2] < building[i] && building[i - 1] < building[i] && building[i + 1] < building[i]);
+			else  continue;
 			for (int j = -2; j <= 2; j++)
 			{
-				if(j!=0)
+				if (j != 0)
 					max = max > building[i + j] ? max : building[i + j];
 			}
-			if (building[i - 2] < building[i] && building[i + 2] < building[i] && building[i - 1] < building[i] && building[i + 1] < building[i])
-			{
-				count += building[i]-max;
-			}
+			count += building[i] - max;
 		}
-		
-		cout << "#" << test_case << " " << count <<"\n";
+
+		cout << "#" << test_case << " " << count << "\n";
 
 
 	}

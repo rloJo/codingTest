45b15eb78a5e7145aaabfa2c3c5fbdb8920c1720
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2023-07-03 11:50:53 +0900

    Create [SW 문제해결 기본] 1일차 - View.cpp

---
+++ SWEA/D3/[SW 문제해결 기본] 1일차 - View.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T,N;
+	int max;
+	int count;
+	T = 10;
+	
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		count = 0;
+		cin >> N;
+		vector<int> building(N);
+		for (int i = 0; i < N; i++)
+			cin >> building[i];
+		for (int i = 2; i < N-2; i++)
+		{
+			max = 0;
+			for (int j = -2; j <= 2; j++)
+			{
+				if(j!=0)
+					max = max > building[i + j] ? max : building[i + j];
+			}
+			if (building[i - 2] < building[i] && building[i + 2] < building[i] && building[i - 1] < building[i] && building[i + 1] < building[i])
+			{
+				count += building[i]-max;
+			}
+		}
+		
+		cout << "#" << test_case << " " << count <<"\n";
+
+
+	}
+	return 0;
+}

f0e3be634814f50927781140895ae294638c41e6
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2023-07-27 15:33:53 +0900

    그리디 알고리즘, 시간을 단축할 수 있는 방법 모색!

---
+++ SWEA/D3/한빈이와 Spot Mart.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	int max;
+	int N, M;
+	
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		max = 0;
+		cin >> N >> M;
+		vector<int> weight(N);
+		for (int i = 0; i < N; i++)
+			cin >> weight[i];
+		for (int i = 0; i < N-1; i++)
+		{
+			for (int j = i + 1; j < N; j++)
+			{
+				if (weight[i] + weight[j] <= M)
+				{
+					max = (weight[i] + weight[j]) > max ? (weight[i] + weight[j]) : max;
+				}
+			}
+		}
+		if(max!=0)
+			cout << "#" << test_case << " " << max << "\n";
+		else
+			cout << "#" << test_case << " " << -1 << "\n";
+	}
+	return 0;
+}

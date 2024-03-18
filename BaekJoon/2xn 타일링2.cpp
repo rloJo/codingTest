7ee855996db3fa452842166a3459b72feb05e2ad
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2024-03-18 09:40:10 +0900

    Create 2xn 타일링2.cpp

---
+++ BaekJoon/2xn 타일링2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int dp[1002];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin >> n;
+	dp[1] = 1;
+	dp[2] = 3;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + (dp[i - 2] * 2))%10007;
+	}
+
+	cout << dp[n];
+	return 0;
+}

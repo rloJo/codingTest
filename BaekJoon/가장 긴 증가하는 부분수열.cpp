58dedade85da45c598805cf7802368c339698148
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2024-03-19 09:47:31 +0900

    Create 가장 긴 증가하는 부분수열.cpp

---
+++ BaekJoon/가장 긴 증가하는 부분수열.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int T,N;
+int arr[1002];
+int dp[1002];
+int ans = 0;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	for (int i = 0; i < N; i++) {
+		dp[i] = 1;
+		for (int j = 0; j <= i; j++) {
+			if (arr[i] > arr[j])
+				dp[i] = max(dp[i], dp[j] + 1);
+			ans = max(ans, dp[i]);
+		}
+	}
+
+	cout << ans << " ";
+	return 0;
+}

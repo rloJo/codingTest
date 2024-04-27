b45ffbb6ee98e42e7ad2a7cea4d68de85333ff71
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2024-04-27 10:50:25 +0900

    Create A와 B2.cpp

---
+++ BaekJoon/A와 B2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string S, T;
+string tmp = "";
+bool flag = false;
+
+void DFS(string s, string t) {
+	if (s == t) {
+		flag = true;
+		return;
+	}
+
+	if (s.length() >= t.length()) return;
+	if (t[t.length() - 1] == 'A') {
+		tmp = t;
+		tmp.erase(tmp.length() - 1);
+		DFS(s, tmp);
+	}
+	if (t[0] == 'B') {
+		tmp = t;
+		tmp.erase(tmp.begin());
+		reverse(tmp.begin(), tmp.end());
+		DFS(s, tmp);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> S >> T;
+	DFS(S, T);
+	if (flag) cout << 1;
+	else cout << 0;
+	return 0;
+}

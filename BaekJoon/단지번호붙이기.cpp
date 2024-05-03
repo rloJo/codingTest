0726f51d761a0abee72708c907a10a37040d2d44
LeeEunWoo <114863642+rloJo@users.noreply.github.com>
2024-05-03 10:11:31 +0900

    Update 단지번호붙이기.cpp

--- BaekJoon/단지번호붙이기.cpp
+++ BaekJoon/단지번호붙이기.cpp
@@ -1,68 +1,65 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#include<algorithm>
-/*
-<그림 1>과 같이 정사각형 모양의 지도가 있다.
-1은 집이 있는 곳을, 0은 집이 없는 곳을 나타낸다.
-철수는 이 지도를 가지고 연결된 집의 모임인 단지를 정의하고, 단지에 번호를 붙이려 한다. 
-여기서 연결되었다는 것은 어떤 집이 좌우, 혹은 아래위로 다른 집이 있는 경우를 말한다.
-대각선상에 집이 있는 경우는 연결된 것이 아니다. 
-<그림 2>는 <그림 1>을 단지별로 번호를 붙인 것이다. 지도를 입력하여 단지수를 출력하고,
-각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력하는 프로그램을 작성하시오.
+#include <algorithm>
 
-첫 번째 줄에는 지도의 크기 N(정사각형이므로 가로와 세로의 크기는 같으며 5≤N≤25)이 입력되고,
-그 다음 N줄에는 각각 N개의 자료(0혹은 1)가 입력된다.
-
-
-*/
+#define X first
+#define Y second
 
 using namespace std;
 
 int N;
-string board[30];
-int vis[30][30];
+string map[26];
+bool vis[26][26];
+vector<int> w;
+
 int dx[4] = { 1,0,-1,0 };
 int dy[4] = { 0,1,0,-1 };
 
+bool OOB(int x, int y) {
+	return x < 0 || x >= N || y < 0 || y >= N;
+}
+
+void bfs(int x, int y){
+	int wide = 0;
+	queue<pair<int, int>> Q;
+	Q.push({ x,y });
+	vis[x][y] = true;
+	wide++;
+	while (!Q.empty()) {
+		pair<int, int> cur = Q.front(); 
+		Q.pop();
+		for (int i = 0; i < 4; i++) {
+			int nx = cur.X + dx[i];
+			int ny = cur.Y + dy[i];
+			if (OOB(nx, ny) || vis[nx][ny]) continue;
+			if (map[nx][ny] == '0') continue;
+			Q.push({ nx,ny }); vis[nx][ny] = true;
+			wide++;
+		}
+	}
+	w.push_back(wide);
+}
 
 int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(0); cout.tie(0);
 	cin >> N;
+	
 	for (int i = 0; i < N; i++)
-		cin >> board[i];
-	vector<int> ans;
-	int count = 0;
+		cin >> map[i];
+
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			if (board[i][j] == '0' || vis[i][j] == 1) continue;
-			queue<pair<int, int>> Q;
-			vis[i][j] = 1;
-			Q.push({ i,j });
-			int w = 1;
-			count++;
-			while (!Q.empty()) {
-				pair<int, int> cur = Q.front(); Q.pop();
-				for (int dir = 0; dir < 4; dir++) {
-					int nx = cur.first + dx[dir];
-					int ny = cur.second + dy[dir];
-					if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-					if (vis[nx][ny] == 1 || board[nx][ny] == '0') continue;
-					Q.push({ nx,ny });
-					vis[nx][ny] = 1;
-					w++;
-				}
-			}
-			ans.push_back(w);
+			if (vis[i][j] || map[i][j] == '0') continue;
+			bfs(i, j);
 		}
 	}
 
-	cout << count << "\n";
-	sort(ans.begin(), ans.end());
-	for (int i = 0; i < ans.size(); i++) {
-		cout << ans[i] << "\n";
-	}
+	sort(w.begin(), w.end());
+	
+	cout << w.size() << "\n";
+	for (auto c : w) cout << c << "\n";
 
 	return 0;
 }
